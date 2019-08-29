4593eaa883b732cf4c7c5d8124e72849d4d1b9c3
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-08-29 15:40:11 -0600

    following along in class in unity

---
+++ project1/Assets/scripts/startBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startBehavior : MonoBehaviour
+{
+    private void Start() {
+        print("hello world");
+    }
+}
