f145bd3c1df28f806e4b88a50c4c04fdf2f92b10
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-09-01 17:30:07 -0600

    picked a fight with collsions, didnt win
    still cant get oncollisionenter to trigger

--- project1/Assets/scripts/player.cs
+++ project1/Assets/scripts/player.cs
@@ -7,27 +7,40 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private int moveSpeed=(1);
+    private bool bump;
     private void Start()
     {
         transform.position = new Vector3(0, 0, 0);
     }
     private void Update()
     {
-        if (Input.GetKey("up")||Input.GetKey("w")) {
-            transform.position += new Vector3(+0, + moveSpeed, +0);
+        if (!bump)
+        {
+            if (Input.GetKey("up") || Input.GetKey("w")) {
+                transform.position += new Vector3(+0, +moveSpeed, +0);
+            }
+            if (Input.GetKey("left") || Input.GetKey("a")) {
+                transform.position += new Vector3(-moveSpeed, +0, +0);
+            }
+            if (Input.GetKey("right") || Input.GetKey("d")) {
+                transform.position += new Vector3(+moveSpeed, +0, +0);
+            }
+            if (Input.GetKey("down") || Input.GetKey("s")) {
+                transform.position += new Vector3(+0, -moveSpeed, +0);
+            }
         }
-        if (Input.GetKey("left")||Input.GetKey("a")) {
-            transform.position += new Vector3(-moveSpeed, +0, +0);
-        }
-        if (Input.GetKey("right")||Input.GetKey("d")) {
-            transform.position += new Vector3(+moveSpeed, +0, +0);
-        }
-        if (Input.GetKey("down")||Input.GetKey("s")) {
-            transform.position += new Vector3(+0, -moveSpeed, +0);
+        else
+        {
+            print(bump);
         }
     }
-    public void OnCollisionEnter(Collision other)
+
+    private void OnCollisionEnter(Collision other)
     {
-        print(other.gameObject.name);
+        bump = true;
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        bump = false;
     }
 }
