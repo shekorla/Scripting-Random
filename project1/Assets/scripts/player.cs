44fc974c8cb802ae560ffcdb4a9c1922220f21dd
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-09-01 18:51:10 -0600

    Won! messy collisons, but collisions, started adding a point system
    figure out how to draw score

--- project1/Assets/scripts/player.cs
+++ project1/Assets/scripts/player.cs
@@ -6,41 +6,47 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    private int moveSpeed=(1);
-    private bool bump;
+    private int moveSpeed=(1);//players move speed
+    private bool bump;//are you currently touching a wall? yes or no
+    //public int score = 0;
     private void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, 0, 0);//start in the center each time
     }
     private void Update()
     {
-        if (!bump)
-        {
-            if (Input.GetKey("up") || Input.GetKey("w")) {
-                transform.position += new Vector3(+0, +moveSpeed, +0);
-            }
-            if (Input.GetKey("left") || Input.GetKey("a")) {
-                transform.position += new Vector3(-moveSpeed, +0, +0);
-            }
-            if (Input.GetKey("right") || Input.GetKey("d")) {
-                transform.position += new Vector3(+moveSpeed, +0, +0);
-            }
-            if (Input.GetKey("down") || Input.GetKey("s")) {
-                transform.position += new Vector3(+0, -moveSpeed, +0);
-            }
+        if (Input.GetKey("up") || Input.GetKey("w")) {
+            transform.position += new Vector3(+0, +moveSpeed, +0);
         }
-        else
+        if (Input.GetKey("left") || Input.GetKey("a")) {
+            transform.position += new Vector3(-moveSpeed, +0, +0);
+        }
+        if (Input.GetKey("right") || Input.GetKey("d")) {
+            transform.position += new Vector3(+moveSpeed, +0, +0);
+        }
+        if (Input.GetKey("down") || Input.GetKey("s")) {
+            transform.position += new Vector3(+0, -moveSpeed, +0);
+        }
+        
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name=="wall")
         {
-            print(bump);
+            moveSpeed=1;//move normal once you leave the wall
         }
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name=="wall")
+        {
+            moveSpeed = -1;//move back out of the wall
+        }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        bump = true;
-    }
-    private void OnCollisionExit(Collision other)
-    {
-        bump = false;
+       /* if (other.gameObject.name=="point")
+        {
+            score += 1;
+        }*/
     }
 }
