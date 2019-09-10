b5624ab2326830ec91c8070e9c37bd20e6283897
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-09-10 15:41:57 -0600

    following along in class, made powerups and damage

---
+++ project1/Assets/scripts/TriggerEvents.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+    }
+}
