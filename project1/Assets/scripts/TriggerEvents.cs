a03571e979eff32bb1aa01d4388b910c12efc0d1
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-09-10 20:45:47 -0600

    messed around with events, made a snail enemy

--- project1/Assets/scripts/TriggerEvents.cs
+++ project1/Assets/scripts/TriggerEvents.cs
@@ -7,5 +7,10 @@ public class TriggerEvents : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         triggerEvent.Invoke();
+        Move(gameObject);
+    }
+    public void Move(GameObject myself)
+    {
+        myself.transform.position=new Vector3(UnityEngine.Random.Range(-23,23),UnityEngine.Random.Range(-7,7),0);
     }
 }
---
+++ project1/Assets/scripts/enemyAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class enemyAction : MonoBehaviour
+{
+    public UnityEvent start;
+
+    public float speed = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += transform.forward*speed;
+    }
+}
