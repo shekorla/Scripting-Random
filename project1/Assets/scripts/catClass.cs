48b55ca00a4d5c51dc2479e3c1651ef2436e1f03
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-09-12 15:46:00 -0600

    following along in class

---
+++ project1/Assets/scripts/catClass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class catClass : MonoBehaviour
+{
+   public UnityEvent catActs;
+
+   private void OnMouseDown()
+   {
+      catActs.Invoke();
+   }
+}
---
+++ project1/Assets/scripts/particleControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleControl : MonoBehaviour
+{
+    public ParticleSystem partycalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+       partycalls.Emit(100); 
+    }
+
+    private void OnMouseDown()
+    {
+        partycalls.Emit(100);
+    }
+}
