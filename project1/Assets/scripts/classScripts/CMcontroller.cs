608d0858559e79ffe92711e4ae8a43205b1d51eb
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-10-15 15:50:03 -0600

    following along in class cinemachine

---
+++ project1/Assets/scripts/classScripts/CMcontroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Cinemachine;
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CMcontroller : MonoBehaviour
+{
+    public playerData pData;
+    private CinemachineVirtualCamera vCamera;
+    void Awake()
+    {
+        vCamera = GetComponent<CinemachineVirtualCamera>();
+        pData.instanceAction = instanceHandler;
+        pData.InstancePlayer();
+    }
+
+    private void instanceHandler(GameObject obj)
+    {
+        vCamera.Follow = obj.transform;
+    }
+}
--- project1/Assets/scripts/classScripts/dataScripts/playerData.cs
+++ project1/Assets/scripts/classScripts/dataScripts/playerData.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 [CreateAssetMenu]
 public class playerData : gameArtData
 {
+    public UnityAction<GameObject> instanceAction;
     public FloatData health;
     public List<weaponData> weapons;
     public clothesData shirt, pants;
@@ -10,8 +13,9 @@ public class playerData : gameArtData
     public void InstancePlayer()
     {
        var newPlayer= Instantiate(prefab);
-       var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
-       newSprite.sprite = sprite;
-       newSprite.color = spriteColor;
+       var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+       playerSprite.sprite = sprite;
+       playerSprite.color = spriteColor;
+       instanceAction(newPlayer);
     }
 }
