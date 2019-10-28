0d9aa33ff6679befeebd757045198cf6d6369d95
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-10-27 21:12:20 -0600

    fully functioning checkpoint system
    little bulky

--- project1/Assets/scripts/Checkpoints.cs
+++ project1/Assets/scripts/Checkpoints.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,22 +10,30 @@ public class Checkpoints : MonoBehaviour
     public CharacterController pChara;
     public Vector3 checkpoint;
     public UnityEvent death;
+    
+    private bool check2 = false;
     private void Start()
     {
         checkpoint=Vector3.zero;
         player=GameObject.FindWithTag("Player");
         pChara = player.GetComponent<CharacterController>();
+        
 
     }
     public void YouDied()
     {
-        pChara.transform.position = checkpoint;
+        pChara.enabled = false;
         player.transform.position=checkpoint;
+        pChara.enabled = true;
         death.Invoke();
     }
-
-    public void updateCheckLoc(GameObject Trigger)
+    private void Update()
     {
-        checkpoint = Trigger.transform.position;
+        if (player.transform.position.x>=120f&&check2==false)
+        {
+            checkpoint = gameObject.transform.position;
+            check2 = true;
+            print("one");
+        }
     }
 }
