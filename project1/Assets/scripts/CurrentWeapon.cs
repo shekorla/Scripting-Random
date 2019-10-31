75703b8bbe2f500b4d1d2e1e8f826d27a5417899
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-10-30 21:38:48 -0600

    simple weapon switch

---
+++ project1/Assets/scripts/CurrentWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentWeapon : MonoBehaviour
+{
+    public SpriteRenderer prenderer;
+    public void changeImage(weaponData weapSprite)
+    {
+        prenderer.sprite = weapSprite.sprite;
+        prenderer.color = weapSprite.spriteColor;
+    }
+}
---
+++ project1/Assets/scripts/WeaponHandle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandle : MonoBehaviour
+{
+    public weaponData weapon;
+
+    public void SwitchWeapon(weaponData newWeapon)
+    {
+        weapon = newWeapon;
+    }
+    
+}
