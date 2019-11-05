cb3b7ab1b7c1bc76786e9b34ab4afc5258a8bff1
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-11-05 15:46:59 -0700

    class nav mesh agents

---
+++ project1/Assets/scripts/classScripts/AI/AiNav.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiNav : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Vector3Data location;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.destination = location.value;
+    }
+}
--- project1/Assets/scripts/classScripts/mouseEvents.cs
+++ project1/Assets/scripts/classScripts/mouseEvents.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 
 public class mouseEvents : MonoBehaviour
 {
     public UnityEvent mouseEvent;
+    private Camera cam;
+    public Vector3Data data;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
     private void OnMouseDown()
     {
-        mouseEvent.Invoke();
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+        {
+            data.value = hit.point;
+        }
+        //mouseEvent.Invoke();
     }
 }
