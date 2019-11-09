0ce2a7f3c1ce607e1894c49049efde14294df418
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-11-09 11:53:09 -0700

    nav mesh room, with switch doors

---
+++ project1/Assets/scripts/TopPlrHandle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopPlrHandle : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3Data mousePos;
+    public Vector3 startPos;
+
+    private playerDataHome data;
+    private SpriteRenderer plrSpr;
+    
+    public void Start()
+    {
+        mousePos.value = startPos;
+        plrSpr=player.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void ChangePlr(playerDataHome newPlr)
+    {
+        plrSpr.sprite = newPlr.pSprite;
+        plrSpr.color = newPlr.pColor;
+
+    }
+}
