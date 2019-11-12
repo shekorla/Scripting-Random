418a3b6aa3b45f81b2af72ab9f5b3ac8e7696eeb
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-11-12 15:44:58 -0700

    class coruoutines

---
+++ project1/Assets/scripts/classScripts/coroutines/coroutines.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class coroutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float seconds =10f;
+    public int counter=10;
+    public bool canRun;
+    private WaitForSeconds wFsObj;
+
+    public void Awake()
+    {
+        wFsObj=new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoruoutien());
+    }
+    IEnumerator RunCoruoutien()
+    {
+        startEvent.Invoke();
+        while (counter>0)
+        {
+            repeatEvent.Invoke();
+            yield return wFsObj;
+            counter--;
+        }
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wFsObj;
+        }
+        endEvent.Invoke();
+    }
+
+}
