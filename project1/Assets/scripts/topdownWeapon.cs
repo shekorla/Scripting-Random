47de58e4272cdcb1566058c6645ad3bebc63d214
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-11-18 20:59:35 -0700

    sword that swings

---
+++ project1/Assets/scripts/topdownWeapon.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class topdownWeapon : MonoBehaviour
+{
+    public GameObject player;
+    public int rotTimeReset;
+    public bool direction;//left is true, right is false
+    public UnityEvent callSpinEve;
+
+    private WaitForSeconds wfsObj;
+    private int spinLeft, spinRight, rotTime;
+    private Vector3 plrLoc;
+    private bool dontCrash;
+
+    private void Start()
+    {
+        wfsObj = new WaitForSeconds(.01f);
+        spinLeft = -10;
+        spinRight = 10;
+        rotTime = rotTimeReset;
+        direction = true;
+        dontCrash = false;
+    }
+
+    public void Update()
+    {
+        plrLoc = player.transform.position;
+        if (Input.GetButtonDown("Jump"))
+        {
+            callSpinEve.Invoke();
+        }
+    }
+
+    public void CallSpin()
+    {
+        StartCoroutine(Spin());
+    }
+    public IEnumerator Spin()
+    {
+        if (dontCrash==false)
+        {
+            dontCrash = true;
+            direction = true;
+            rotTime = rotTimeReset;
+            while (rotTime>0)
+            {
+                if (direction==true)
+                {
+                    transform.RotateAround(plrLoc, Vector3.up, spinLeft);
+                    rotTime--;
+                    yield return wfsObj;
+                }
+            }
+            rotTime = rotTimeReset;
+            direction = false;
+            while (rotTime>0)
+            {
+                if (direction==false)
+                {
+                    transform.RotateAround(plrLoc, Vector3.up, spinRight);
+                    rotTime--;
+                    yield return wfsObj;
+                }
+            }
+            direction=true;
+            dontCrash = false;
+        }
+    }
+}
