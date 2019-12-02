a6005e050c80a6bff5e1c150f338e9dd68aa3694
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-12-02 15:41:38 -0700

    used animator to create an elevator number thing
    use that to make a checkpoint sys

---
+++ project1/Assets/scripts/TwCheckpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TwCheckpoint : MonoBehaviour
+{
+    public UnityEvent dead;
+    public CharacterController cont;
+    public Vector3Data checkPLoc;
+
+    private void Start()
+    {
+        checkPLoc.value=new Vector3(-6,-3,0);
+    }
+
+    public void YouDied()
+    {
+        cont.enabled = false;
+        gameObject.transform.position=checkPLoc.value;
+        cont.enabled = true;
+        dead.Invoke();
+    }
+}
