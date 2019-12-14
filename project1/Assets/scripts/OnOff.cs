09441599037faaabc415302540834a578e2cca8e
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-12-14 13:12:23 -0700

    elevator indicator more or less works

---
+++ project1/Assets/scripts/OnOff.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OnOff : MonoBehaviour
+{
+    public void Swap()
+    {
+        if (gameObject.active==true)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
