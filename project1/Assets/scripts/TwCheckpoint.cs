c2c8745c98e396a109c9b73cd67f35e16fee89e3
kassidy mcconkie <kassidy.mcconkie@gmail.com>
2019-12-17 12:36:13 -0700

    Values update, but teleportation is off.

--- project1/Assets/scripts/TwCheckpoint.cs
+++ project1/Assets/scripts/TwCheckpoint.cs
@@ -6,18 +6,36 @@ public class TwCheckpoint : MonoBehaviour
 {
     public UnityEvent dead;
     public CharacterController cont;
-    public Vector3Data checkPLoc;
+    public Vector3 currentCK;
+    public Vector3Data startPoint;
+    public FloatData health;
 
     private void Start()
     {
-        checkPLoc.value=new Vector3(-6,-3,0);
+        currentCK = startPoint.value;
     }
 
     public void YouDied()
     {
+        print("dead");
         cont.enabled = false;
-        gameObject.transform.position=checkPLoc.value;
+        gameObject.transform.position=currentCK;
         cont.enabled = true;
         dead.Invoke();
+    }
+
+    public void UpdateCK(Vector3Data newLoc)
+    {
+        currentCK = newLoc.value;
+        print(currentCK);
+    }
+
+    private void Update()
+    {
+        if (health.value<=0)
+        {
+            YouDied();
+            health.value = 1f;
+        }
     }
 }
